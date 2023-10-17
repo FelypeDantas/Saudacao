643f8fa234725b2f26c190a11c770e78946653c9
Felype <132705654+FelypeDantas@users.noreply.github.com>
2023-10-17 09:25:40 -0300

    Add files via upload

---
+++ ProgramaIgor.java
@@ -0,0 +1,11 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class ProgramaIgor {
+  public static void main(String args []) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Digite seu nome: ");
+	        String nome = sc.next();
+	        System.out.println( "Olá "+ nome);
+	      }
+	  }
